0e62e504161c9dbec666b77c96d0162c5daca13f
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2024-04-16 14:33:23 +0200

    finished AuD/Übung1/TailRecursion

--- algorithmen und datenstrukturen/Übung/Übung1/TailRecursion.java
+++ algorithmen und datenstrukturen/Übung/Übung1/TailRecursion.java
@@ -1,12 +1,34 @@
 package Übung1;
 
+//import aud.*;
+
 public class TailRecursion {
-    public static int pow2(int n){
-        return n;
+    public static int pow2(int n) {
+        return _pow2(n, 1);
+    }
+
+    private static int _pow2(int exponent, int result) {
+        if (exponent == 0) {
+            return result;
+        } else {
+            return _pow2(exponent - 1, result * 2);
+        }
     }
 
     	
-    public static int sumFactors(int n){
-        return n;
+    public static int sumFactors(int n) {
+        return sumFactorsHelper(n, 1, 0);
+    }
+
+    private static int sumFactorsHelper(int n, int currentDivisor, int sum) {
+        if (currentDivisor >= n) {
+            return sum;
+        } else {
+            if (n % currentDivisor == 0) {
+                return sumFactorsHelper(n, currentDivisor + 1, sum + currentDivisor);
+            } else {
+                return sumFactorsHelper(n, currentDivisor + 1, sum);
+            }
+        }
     }
 }
