304fb18ebffc82d0846b24ba3b5d4d43a63edfcc
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2024-04-16 22:18:40 +0200

    updated AuD/TPalindrome.java

--- algorithmen und datenstrukturen/Übung/Übung2/TPalindrome.java
+++ algorithmen und datenstrukturen/Übung/Übung2/TPalindrome.java
@@ -5,50 +5,68 @@ import aud.Stack;
 public class TPalindrome {
     public static boolean isTPalindrome(String text) {
         Stack<Character> stack = new Stack<>();
-
+    
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
-
+    
             switch (c) {
                 case '(':
-                case '*':
+                    // Einfach eine öffnende Klammer auf den Stapel legen
                     stack.push(c);
                     break;
                 case ')':
+                    // Hier überprüfen wir den Inhalt zwischen den Klammern
                     StringBuilder temp = new StringBuilder();
                     while (!stack.is_empty() && stack.top() != '(') {
-                        temp.append(stack.pop());
+                        temp.insert(0, stack.pop()); // Fügen Sie Zeichen am Anfang ein
                     }
-                    if (stack.is_empty() || stack.pop() != '(') {
+                    
+                    if (stack.is_empty()) {
                         return false; // Ungültige Klammerung
                     }
-                    if (isPalindrome(temp.toString())) {
-                        stack.push('*');
-                    } else {
+                    
+                    // Die öffnende Klammer entfernen
+                    stack.pop();
+    
+                    if (!isPalindrome(temp.toString())) {
                         return false;
                     }
+                    // Ein gültiges Palindrom in Klammern wurde gefunden
+                    // Wir behandeln es als ein Stern (*) im Text
+                    if (!stack.is_empty() && stack.top() == '*') {
+                        // Wenn das Top-Element ein Stern ist, entferne es
+                        stack.pop();
+                    } else {
+                        // Andernfalls füge einen Stern hinzu
+                        stack.push('*');
+                    }
                     break;
+                case '*':
+                    // Sterne dürfen nicht direkt im Text stehen
+                    return false;
                 default: // Für a-Z
                     if (!Character.isAlphabetic(c)) {
                         return false; // Ungültiges Zeichen gefunden
                     }
                     if (!stack.is_empty() && stack.top() == '*') {
+                        // Wenn das Top-Element ein Stern ist, entferne es
                         stack.pop();
                     } else {
+                        // Andernfalls füge das Zeichen hinzu
                         stack.push(c);
                     }
             }
         }
-
+    
         // Überprüfen, ob der verbleibende Stapelinhalt ein Palindrom ist
         StringBuilder remaining = new StringBuilder();
         while (!stack.is_empty()) {
-            remaining.append(stack.pop());
+            remaining.insert(0, stack.pop());
         }
-
+    
         return isPalindrome(remaining.toString());
     }
-
+    
     private static boolean isPalindrome(String str) {
         int left = 0;
         int right = str.length() - 1;
@@ -62,7 +80,7 @@ public class TPalindrome {
     
     // Testmethode zum Ausführen der Prüfung
     public static void main(String[] args) {
-        String text = "abc(ah*v*ha)cba"; // Beispieltext für einen T-Palindrom
+        String text = "(abc(aha)(u)cba)"; // Beispieltext für einen T-Palindrom
         System.out.println(text + " : " + isTPalindrome(text));
     }
 }
