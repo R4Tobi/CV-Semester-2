703f208276ca1a00ec2bab68ea2e5900879da04b
R4Tobi <R4Tobi@users.noreply.github.com>
2024-04-20 12:27:17 +0200

    updated AuD/aud

---
+++ Algorithmen und Datenstrukturen/Übung/aud/util/Graphvizable.java
@@ -0,0 +1,17 @@
+package aud.util;
+
+/**
+ * Interface for <a href="http://www.graphviz.org/">GraphViz</a>
+ * rendering.
+ * 
+ * @see DotViewer
+ */
+public interface Graphvizable {
+
+    /**
+     * Get <a href="http://www.graphviz.org/content/dot-language">dot</a>
+     * representation.
+     * <p>
+     */
+    String toDot();
+}
