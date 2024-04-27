346669ba7892e88429f500eea2a3ebed70dd4127
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2024-04-27 20:19:46 +0200

    added AuD/Übung2/adHoc

---
+++ algorithmen und datenstrukturen/Übung/Übung2/SortList.java
@@ -0,0 +1,28 @@
+package Übung2;
+
+import aud.SList;
+
+public class SortList<T extends Comparable<T>> {
+    SList<T> li;
+
+    /**
+     * Erzeugt eine neue Instanz von SortList.
+     */
+    public SortList() {
+        this.li = new SList<>();
+    }
+
+    /**
+     * Fügt ein Element in die sortierte Liste ein, falls es noch nicht vorhanden ist.
+     * @param obj Das einzufügende Objekt.
+     * @return true, wenn das Element eingefügt wurde, sonst false.
+     */
+    public boolean insert(T obj) {
+        if (this.li.empty()) { // Überprüfe ob die Liste leer ist
+            this.li.push_front(obj);
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
