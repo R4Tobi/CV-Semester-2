cc1b0aa510a9dd36d537f3f821f13fb988bfd901
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2024-05-27 11:16:11 +0200

    .

---
+++ algorithmen und datenstrukturen/Übung/aud/example/expr/ExpressionParser2.java
@@ -0,0 +1,139 @@
+package aud.example.expr;
+
+import aud.Queue;
+
+/**
+ * Same as {@link ExpressionParser} but using modified grammar to provide
+ * the usual <b>left-associative</b> expressions.
+ * <p>
+ * 
+ * Here is the "correct" grammar, that implements left-associative expressions,
+ * i.e., {@code a+b+c=(a+b)+c} and {@code a*b*c=(a*b)*c}.
+ * 
+ * <pre>
+ * <code>
+ expression ::= sum
+
+ sum        ::= product |
+                sum '+' product | sum '-' product
+
+ product    ::= factor |
+                product '*' factor | product '/' factor
+
+ factor     ::= number | symbol | '(' expression ')' |
+                '+' factor | '-' factor
+ </code>
+ * </pre>
+ * 
+ * <b>Important note:</b> We cannot implement this directly with a recursive
+ * descent parser as this would result in an infinite recursion! (This
+ * type of recursion is generally referred to as <em>left recursion</em> and
+ * requires a transformation of the grammar.)
+ * <p>
+ * 
+ * I don't go into details but provide a simple solution right ahead:
+ * model part of the recursion (the <em>left recursion</em>) using an
+ * iterative implementation. (Can you see how the transformed grammar
+ * would look like?)
+ * 
+ * @see ExpressionParser
+ */
+public class ExpressionParser2 extends ExpressionParser {
+
+    //
+    // Here is the implementation of BNF rules as methods:
+    //
+
+    /**
+     * parse sum
+     * <p>
+     * 
+     * <pre>
+     * <code>
+     sum ::= product |
+             sum  '+' product | sum '-' product
+     </code>
+     * </pre>
+     */
+    @Override
+    protected ExpressionTree sum(int level) {
+        verbose(level, "<sum>");
+
+        // temporary storage
+        Queue<ExpressionTree> expr = new Queue<ExpressionTree>();
+        Queue<Integer> op = new Queue<Integer>();
+
+        ExpressionTree tree = product(level + 1);
+
+        // parse iteratively
+        while (lookahead() == Tokenizer.PLUS || lookahead() == Tokenizer.MINUS) {
+            op.enqueue(lookahead());
+            nextToken();
+            expr.enqueue(product(level + 1));
+        }
+
+        // setup tree
+        while (!op.is_empty()) {
+            if (op.dequeue() == Tokenizer.PLUS)
+                tree = new ExpressionTree(new Plus(), tree, expr.dequeue());
+            else
+                tree = new ExpressionTree(new Minus(), tree, expr.dequeue());
+        }
+        return tree;
+    }
+
+    /**
+     * parse product
+     * <p>
+     * 
+     * <pre>
+     * <code>
+     product ::= factor |
+                 product '*' factor | product '/' factor
+     </code>
+     * </pre>
+     */
+    protected ExpressionTree product(int level) {
+        verbose(level, "<product>");
+
+        // temporary storage
+        Queue<ExpressionTree> expr = new Queue<ExpressionTree>();
+        Queue<Integer> op = new Queue<Integer>();
+
+        ExpressionTree tree = factor(level + 1);
+
+        // parse iteratively
+        while (lookahead() == Tokenizer.TIMES || lookahead() == Tokenizer.DIVIDE) {
+            op.enqueue(lookahead());
+            nextToken();
+            expr.enqueue(factor(level + 1));
+        }
+
+        // setup tree
+        while (!op.is_empty()) {
+            if (op.dequeue() == Tokenizer.TIMES)
+                tree = new ExpressionTree(new Times(), tree, expr.dequeue());
+            else
+                tree = new ExpressionTree(new Divide(), tree, expr.dequeue());
+        }
+        return tree;
+    }
+
+    /** test and example for usage */
+    public static void main(String[] args) {
+
+        String input = (args.length > 0) ? args[0] : "a*b*c";
+
+        System.out.println("input = '" + input + "'");
+
+        ExpressionParser2 p = new ExpressionParser2();
+        p.setVerbose(true);
+        ExpressionTree tree = p.parse(input);
+
+        System.out.println("output = '" + tree + "'");
+
+        System.out.println(tree.postorder().toString());
+
+        System.out.println(tree.toTikZ());
+    }
+}
--- algorithmen und datenstrukturen/Übung/Übung6/SimpleTree.java
+++ algorithmen und datenstrukturen/Übung/Übung6/SimpleTree.java
@@ -1,48 +1,75 @@
 package Übung6;
-//-----------------------------------------------------------------//
+
+/**
+ * Eine einfache Implementierung eines binären Suchbaums.
+ * 
+ * @param <T> der Typ der Elemente, die im Baum gespeichert sind, muss das
+ *            Comparable-Interface implementieren
+ */
 public class SimpleTree<T extends Comparable<T>> {
-    // ---------------------------------------------------------------//
+    /**
+     * Stellt einen Knoten im binären Suchbaum dar.
+     */
     public class Node {
+        // Die Daten für den Knoten
         private T data;
+        // Der linke Kindknoten
         private Node left;
+        // Der rechte Kindknoten
         private Node right;
 
+        /**
+         * Erstellt einen neuen Knoten mit den gegebenen Daten.
+         * 
+         * @param data die Daten, die im Knoten gespeichert werden sollen
+         */
         public Node(T data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
 
+        // Getter für die Daten des Knotens
         public T getData() {
             return data;
         }
 
+        // Getter für das linke Kind des Knotens
         public Node getLeft() {
             return left;
         }
 
+        // Getter für das rechte Kind des Knotens
         public Node getRight() {
             return right;
         }
 
+        // Setter für das linke Kind des Knotens
         public void setLeft(Node left) {
             this.left = left;
         }
 
+        // Setter für das rechte Kind des Knotens
         public void setRight(Node right) {
             this.right = right;
         }
     }
 
-    // ---------------------------------------------------------------//
+    // Die Wurzel des Baums
     private Node root_;
 
-    // ---------------------------------------------------------------//
+    /**
+     * Konstruiert einen leeren binären Suchbaum.
+     */
     public SimpleTree() {
         root_ = null;
     }
 
-    // ---------------------------------------------------------------//
+    /**
+     * Fügt ein Element zum binären Suchbaum hinzu.
+     * 
+     * @param obj das hinzuzufügende Element
+     */
     public void add(T obj) {
         if (root_ == null) {
             root_ = new Node(obj);
@@ -51,6 +78,9 @@ public class SimpleTree<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * Hilfsmethode zum rekursiven Hinzufügen eines Elements.
+     */
     private void addRecursive(Node node, T obj) {
         if (obj.compareTo(node.getData()) < 0) {
             if (node.getLeft() == null) {
@@ -67,11 +97,19 @@ public class SimpleTree<T extends Comparable<T>> {
         }
     }
 
-    // ---------------------------------------------------------------//
+    /**
+     * Überprüft, ob der binäre Suchbaum ein gegebenes Element enthält.
+     * 
+     * @param obj das zu überprüfende Element
+     * @return true, wenn das Element gefunden wird, sonst false
+     */
     public boolean contains(T obj) {
         return containsRecursive(root_, obj);
     }
 
+    /**
+     * Hilfsmethode zum rekursiven Durchsuchen auf ein Element.
+     */
     private boolean containsRecursive(Node node, T obj) {
         if (node == null) {
             return false;
@@ -86,11 +124,18 @@ public class SimpleTree<T extends Comparable<T>> {
         }
     }
 
-    // ---------------------------------------------------------------//
+    /**
+     * Gibt eine String-Repräsentation des binären Suchbaums zurück.
+     * 
+     * @return eine String-Repräsentation des Baums
+     */
     public String toString() {
         return toStringRecursive(root_);
     }
 
+    /**
+     * Hilfsmethode zur Erstellung einer Inorder-String-Repräsentation des Baumes.
+     */
     private String toStringRecursive(Node node) {
         if (node == null) {
             return "";
@@ -107,7 +152,7 @@ public class SimpleTree<T extends Comparable<T>> {
     public static void main(String[] args) {
         SimpleTree<Integer> tree = new SimpleTree<Integer>();
 
-        // Test adding elements to the tree
+        // Test: Elemente zum Baum hinzufügen
         tree.add(5);
         tree.add(3);
         tree.add(7);
@@ -116,11 +161,11 @@ public class SimpleTree<T extends Comparable<T>> {
         tree.add(6);
         tree.add(8);
 
-        // Test contains method
-        System.out.println(tree.contains(5)); // Output: true
-        System.out.println(tree.contains(10)); // Output: false
+        // Test: contains-Methode
+        System.out.println(tree.contains(5)); // Ausgabe: true
+        System.out.println(tree.contains(10)); // Ausgabe: false
 
-        // Test toString method
-        System.out.println(tree.toString()); // Output: 2 3 4 5 6 7 8
+        // Test: toString-Methode
+        System.out.println(tree.toString()); // Ausgabe: 2
     }
 }
